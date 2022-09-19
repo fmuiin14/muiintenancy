622aed08701b6f7d7fdefd897bb96d9e9adceb93
fmuiin14 <fmuiin14@gmail.com>
2022-09-19 14:59:41 +0700

    tenant register done

---
+++ app/Jobs/CreateTenantAdmin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Models\Tenant;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class CreateTenantAdmin implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Tenant $tenant) {
+        $this->tenant=$tenant;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $this->tenant->run(function ($tenant) {
+            // $tenant->password = bcrypt($tenant->password);
+            User::create($tenant->only('name', 'email', 'password'));
+        });
+    }
+}
